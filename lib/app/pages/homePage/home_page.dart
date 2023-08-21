6169d99eb5c1b0f1068bd2b66b2884a1b03d2011
RebecaAlvesLima <cc21448@g.unicamp.br>
2023-08-21 18:06:32 -0300

    Home page teste

---
+++ lib/app/pages/homePage/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Color(0xfff5f5f5),
+      actions: [
+        IconButton(
+          icon: Icon(Icons),
+        )
+      ],
+    );
+  }
+}
