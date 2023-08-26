0da420a284acdc929056291afe22508551c9958a
RebecaAlvesLima <cc21448@g.unicamp.br>
2023-08-25 21:48:19 -0300

    iconButtons

--- lib/app/pages/homePage/home_page.dart
+++ lib/app/pages/homePage/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tcc_me_adote/app/pages/splash/splash_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,13 +11,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Color(0xfff5f5f5),
-      actions: [
-        IconButton(
-          icon: Icon(Icons.account_balance_wallet),
-        )
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        width: 45,
+                        height: 45,
+                        child: Align(
+                          alignment: Alignment.topRight,
+                          child: IconButton(
+                            icon: Icon(Icons.notifications_none),
+                            color: Colors.grey,
+                            onPressed: ((() {})),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Container(
+                    width: 45,
+                    height: 45,
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        icon: Icon(Icons.person),
+                        color: Colors.blue,
+                        onPressed: (() {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Splash()),
+                          );
+                        }),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
