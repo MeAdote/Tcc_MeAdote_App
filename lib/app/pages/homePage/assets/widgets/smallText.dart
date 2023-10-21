2be704094f33aeca3694c15e0081ab9003d7652a
RebecaAlvesLima <cc21448@g.unicamp.br>
2023-10-21 13:16:26 -0300

    mudanças

---
+++ lib/app/pages/homePage/assets/widgets/smallText.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class SmallText extends StatelessWidget {
+  final Color? color;
+  final String text;
+  double size;
+  double height;
+
+  SmallText(
+      {Key? key,
+      this.color = const Color(0xff656565),
+      required this.text,
+      this.size = 12,
+      this.height = 1.2})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+          fontFamily: 'Inter', color: color, fontSize: size, height: height),
+    );
+  }
+}
